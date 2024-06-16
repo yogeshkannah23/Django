ae067c16b12d96a742a80c962f9961fffb0b597c
yogeshkannah23 <yogeshkannahk@gmail.com>
2024-06-16 16:14:08 +0530

    third commit

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_FILES_DIR = [
+    'myblog/static/css',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- myapp/myblog/urls.py
+++ myapp/myblog/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
 
+app_name = "myblog"
 
 urlpatterns = [
     path('',views.index,name="index"),
     path('blogs/<int:id>',views.blogs,name = "blogs"),
+    path('blog/<int:id>',views.detail,name="detail"),
     path('old_url',views.old_url, name="old_url" ),
-    path('new_view',views.new_view,name='new_url'),
+    path('new_view_da',views.new_view,name='new_page_url'),
 ]
